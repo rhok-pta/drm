27d82355c9f7c8f1578df68c9ec77013a474dda3
Jossekin Beilharz <beilharz@acm.org>
2011-12-03 21:23:08 +0200

    whitespace fun

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ var config = {port: process.env.PORT || 8080};
 //
 var app = express.createServer();
 app.configure(function(){
-	app.set('view engine', 'jade');	
-	app.use(express.bodyParser());
-	app.use(express.cookieParser());
-	app.use(express.query());
-	app.use(express.static(__dirname + '/public', {maxAge: 3600000}));
-	app.use(express.session({
-    	store: new MemoryStore({
-      		maxAge: 24 * 60 * 60 * 1000
-    	}),
-		secret: "asdsa"
+  app.set('view engine', 'jade');  
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.query());
+  app.use(express.static(__dirname + '/public', {maxAge: 3600000}));
+  app.use(express.session({
+      store: new MemoryStore({
+          maxAge: 24 * 60 * 60 * 1000
+      }),
+    secret: "asdsa"
  }));
-	app.use(app.router, database);	
+  app.use(app.router, database);  
 });
 
 var routes = require('./routes.js').addRoutes(app,database);
