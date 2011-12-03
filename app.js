6c62b334940f82ba8480aa623464a8e53a40b4e2
Sebastian Woinar <sebastian@woinar.de>
2011-12-03 18:14:16 +0200

    fixed routes + add, added sessions

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var MemoryStore = express.session.MemoryStore;
 
 var database = require('./database.js');
 
@@ -8,21 +9,21 @@ var config = {port: 8080};
 //
 var app = express.createServer();
 app.configure(function(){
-	app.set('view engine', 'jade');
-	
+	app.set('view engine', 'jade');	
 	app.use(express.bodyParser());
 	app.use(express.cookieParser());
 	app.use(express.query());
 	app.use(express.static(__dirname + '/public', {maxAge: 3600000}));
 	app.use(express.session({
-    	store: new MongooseStore({
+    	store: new MemoryStore({
       		maxAge: 24 * 60 * 60 * 1000
-    })
-  }))
-	app.use(app.router, database);
+    	}),
+		secret: "asdsa"
+ }));
+	app.use(app.router, database);	
 });
 
-var routes = require('./routes.js').addRoutes(app);
+var routes = require('./routes.js').addRoutes(app,database);
 
 console.log("Running on port: " + config.port);
 app.listen(config.port);
