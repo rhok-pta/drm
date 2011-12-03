09b522ebee27276f007473c8e42f6d86ebe8b1f2
Sebastian Woinar <sebastian@woinar.de>
2011-12-03 17:23:59 +0200

    added step definitions

--- features/step_definitions/input.js
+++ features/step_definitions/input.js
@@ -7,13 +7,19 @@ var steps = function() {
 	this.When(/^I fill in (.*)$/, function(desc, callback) {
 		switch (desc) {
 			case 'the data of the example user':
-				
+				user.find({})
 		}
-
-
 	});
-}
+	
+	this.When(/^I fill in (.*) to (.*)$/, function(name, value, callback) {
+		switch (name) {
+			case 'username field':
+				throw "todo";
+		}
+	});
 
+
+}
 module.exports = steps;
 
 
