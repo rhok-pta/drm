8c27cce28986290054bb6ac67e32a37574281a3e
Jossekin Beilharz <beilharz@acm.org>
2011-12-03 20:57:38 +0200

    whitespace fun

--- routes.js
+++ routes.js
@@ -1,73 +1,85 @@
 exports.addRoutes = function(app,database) {
-	app.get('/', function(req, res) {
-		res.render("index");
-	});
+    app.get('/', function(req, res) {
+        res.render("index");
+    });
 
-	app.get('/requests', function(req, res) {
-		database.DonationRequest.find({}, function(err, requests) {
-			res.render("requests/index", {requests: requests});
-		});
-	});
+    app.get('/requests', function(req, res) {
+        database.DonationRequest.find({}, function(err, requests) {
+            res.render("requests/index", {requests: requests});
+        });
+    });
 
-	app.get('/requests/:id', function(req, res) {
-		database.DonationRequest.findOne({_id: req.params.id}).populate('donors').populate('groups').run(function(err, request) {
-			if (err)
-				throw err
-			else if (!request)
-				res.send("Could not find request: " + req.params.id);
-			else	
-				res.render("requests/show", {request: request});
-		});
-	});
+    app.get('/requests/:id', function(req, res) {
+        database.DonationRequest.findOne({_id: req.params.id}).populate('donors').populate('groups').run(function(err, request) {
+            if (err)
+                throw err
+            else if (!request)
+                res.send("Could not find request: " + req.params.id);
+            else    
+                res.render("requests/show", {request: request});
+        });
+    });
+    
+    app.get('/posts/new', function(req, res) {
+        var post = new database.Post();
+        post.errors = [];
+        res.render("postNew", {post : post});
+    });
+    
+    app.post('/posts/new', function(req, res) {
+        debugger;
+        var post = database.Post(req.body.post);
+        res.render("donor", {donor : post.donor});
+    });
 
-	app.get('/donors', function(req, res) {
-		database.Donor.find({}, function(err, donors) {
-			res.render("donors", {donors: donors});
-		});
-	});
+    app.get('/donors', function(req, res) {
+        database.Donor.find({}, function(err, donors) {
+            res.render("donors", {donors: donors});
+        });
+    });
 
-	app.get('/donors/new', function(req, res) {
-		var donor = new database.Donor();
-		donor.errors = [];
-		res.render("donorNew", {donor : donor});
-	});
-	
-	app.post('/donors/new', function(req, res) {
-		debugger;
-		var donor = database.Donor(req.body.donor);
-		console.dir(donor);	
-		res.render("donorNew", {donor : donor});
-	});
+    app.get('/donors/new', function(req, res) {
+        var donor = new database.Donor();
+        donor.errors = [];
+        res.render("donorNew", {donor : donor});
+    });
+    
+    app.post('/donors/new', function(req, res) {
+        debugger;
+        var donor = database.Donor(req.body.donor);
+        console.dir(donor); 
+        res.render("donorNew", {donor : donor});
+    });
 
-	app.get('/donors/:id', function(req, res) {
-		database.Donor.findOne({_id: req.params.id}).populate('communicationLog').run(function(err, donor) {
-			if (err)
-				throw err
-			else if (!donor)
-				res.send("Could not find donor: " + req.params.id);
-			else
-				res.render("donor", {donor: donor});
-		});
-	});
+    app.get('/donors/:id', function(req, res) {
+        database.Donor.findOne({_id: req.params.id}).populate('communicationLog').run(function(err, donor) {
+            if (err)
+                throw err
+            else if (!donor)
+                res.send("Could not find donor: " + req.params.id);
+            else
+                res.render("donor", {donor: donor});
+        });
+    });
 
-	app.get('/groups', function(req, res) {
-		database.Group.find({}, function(err, groups) {
-			res.render("groups/index", {groups: groups});
-		});
-	});
+    app.get('/groups', function(req, res) {
+        database.Group.find({}, function(err, groups) {
+            res.render("groups/index", {groups: groups});
+        });
+    });
 
-	app.get('/groups/:id', function(req, res) {
-		database.Group.findOne({_id: req.params.id}).populate('donors').run(function(err, group) {
-			if (err)
-				throw err
-			else if (!group)
-				res.send("Could not find group: " + req.params.id);
-			else	
-				res.render("groups/show", {group: group});
-		});
-	});
+    app.get('/groups/:id', function(req, res) {
+        database.Group.findOne({_id: req.params.id}).populate('donors').run(function(err, group) {
+            if (err)
+                throw err
+            else if (!group)
+                res.send("Could not find group: " + req.params.id);
+            else    
+                res.render("groups/show", {group: group});
+        });
+    });
 
-	app.get('/settings', function(req, res) {
-		res.render("settings");
-	});
+    app.get('/settings', function(req, res) {
+        res.render("settings");
+    });
 };
